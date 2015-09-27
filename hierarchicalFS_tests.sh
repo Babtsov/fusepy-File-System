77fbe0bcdf7d928a013d2c5847fb860c18f44d33
Ben Babtsov <ben.babtsov@ufl.edu>
2015-09-27 00:43:06 -0400

    include more tests for the file system

--- hierarchicalFS_tests.sh
+++ hierarchicalFS_tests.sh
@@ -1,15 +1,40 @@
 cd ./fusemount
-echo "----- recursive directory test -----------"
+echo "------------------------- File system tests -----------------------------"
+echo "Make directories and subdirectories"
+# create 2 directories
 mkdir dir1
 mkdir dir2
+# add content to dir2
+mkdir dir2/dir21
+touch dir2/file1.txt
+echo "I'm in dir2!" > dir2/coco.txt
+# add content to dir1 by 
 cd ./dir1
-touch look
+touch look.txt
+echo "I'm in dir1" > here.txt
 mkdir dir11
 mkdir dir12
 cd ./dir12
 touch here
+# return back to root and print file system
 cd ../..
 echo "Contents of the file system:"
 pwd
 ls -R
-echo "----- END recursive directory test -------"
+
+echo
+echo "Rename dir2 to DIR2 and move ./dir1/look.txt from dir1 to ./DIR2/dir21/"
+mv dir2 DIR2
+mv ./dir1/look.txt ./DIR2/dir21/
+echo "Contents of the file system:"
+pwd
+ls -R
+
+echo
+echo "Recursively remove DIR2 files and contents"
+rm -R DIR2
+echo "Contents of the file system:"
+pwd
+ls -R
+echo "------------------------- END File system tests --------------------------"
+
