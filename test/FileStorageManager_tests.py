b7f77b7a03d41a6924226c33a3cafdf0cb9745c7
Ben Babtsov <ben.babtsov@ufl.edu>
2015-10-16 22:26:33 -0400

    Update FileStorageManager_tests per new API

--- test/FileStorageManager_tests.py
+++ test/FileStorageManager_tests.py
@@ -17,19 +17,20 @@ db_url,db_port = 'localhost',27027
 clear_db(db_url,db_port) # clear the db before the test cases
 
 storage_manager = FileStorageManager(db_url,db_port,5)
-
+root_dict = storage_manager.lookup('/')
 print "################## TEST 1 ###################"
 print "Creates 2 new files and adds them under root"
 loco_dict = storage_manager.insert_file(dict(name='Loco',type='reg',meta='M',data='HEY!'))
 dir1_dict = storage_manager.insert_file(dict(name='dir1',type='dir',meta='M',data={}))
-storage_manager.update_dir_data('/',loco_dict,'$add')
-storage_manager.update_dir_data('/',dir1_dict,'$add')
+storage_manager.update_dir_data(root_dict,loco_dict,'$add')
+storage_manager.update_dir_data(root_dict,dir1_dict,'$add')
 print_db_and_cache(storage_manager)
 
 print "################## TEST 2 ###################"
 print "Removes dir1 from the file system"
-removeddir1_dict = storage_manager.remove_file('/dir1')
-storage_manager.update_dir_data('/',removeddir1_dict,'$remove')
+dir1_dict = storage_manager.lookup('/dir1')
+storage_manager.remove_file(dir1_dict)
+storage_manager.update_dir_data(root_dict,dir1_dict,'$remove')
 print_db_and_cache(storage_manager)
 
 clear_db(db_url,db_port) # clear the db after the test cases
